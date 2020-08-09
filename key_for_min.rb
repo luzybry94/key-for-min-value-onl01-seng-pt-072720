d9e4a8a584302e06b82b3c2e1efcbb645008496f
Chaim Bryski <luzybry94@gmail.com>
2020-08-09 06:20:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 100
   lowest_key = ""
   name_hash.collect do |key, value|
-    if name_hash = {}
-      lowest_key = nil
-    else 
+    if
       value < min_value
       lowest_key = key
     end
